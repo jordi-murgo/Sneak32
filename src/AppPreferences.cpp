6d4ca3b4658beca9e5e703161bd2fd7b798e5534
jordijosep.murgo.contractor <jordijosep.murgo.contractor@bbva.com>
2024-11-01 23:11:18 +0100

    Refactor AppPreferences to add and update preferences for WiFi Pwr, BLE Pwr, CPU speed, and LED settings

--- src/AppPreferences.cpp
+++ src/AppPreferences.cpp
@@ -1,6 +1,11 @@
 #include "AppPreferences.h"
 #include <Arduino.h>
 
+// For TX Power Default Values
+#include <WiFi.h>
+#include <esp_bt.h>
+
+
 Preferences preferences;
 AppPreferencesData appPrefs;
 
@@ -21,11 +26,16 @@ void printPreferences() {
     Serial.printf(" - stealth_mode: %s\n", appPrefs.stealth_mode ? "true" : "false");
     Serial.printf(" - authorized_address: %s\n", appPrefs.authorized_address);
 
-    Serial.printf(" - loop_delay: %u ms\n", appPrefs.loop_delay);
+    Serial.printf(" - wifi_channel_dwell_time: %u ms\n", appPrefs.wifi_channel_dwell_time);
     Serial.printf(" - only_mgmt: %s\n", appPrefs.only_management_frames ? "true" : "false");
-    Serial.printf(" - ble_scan_period: %u ms\n", appPrefs.ble_scan_period);
+    Serial.printf(" - ble_scan_delay: %u s\n", appPrefs.ble_scan_delay);
     Serial.printf(" - ble_scan_duration: %u s\n", appPrefs.ble_scan_duration);
     Serial.printf(" - ignore_random_ble: %s\n", appPrefs.ignore_random_ble_addresses ? "true" : "false");
+
+    Serial.printf(" - cpu_speed: %u\n", appPrefs.cpu_speed);
+    Serial.printf(" - led_mode: %u\n", appPrefs.led_mode);
+    Serial.printf(" - wifi_tx_power: %u\n", appPrefs.wifiTxPower);
+    Serial.printf(" - ble_tx_power: %u\n", appPrefs.bleTxPower);
 }
 
 void loadAppPreferences() {
@@ -50,10 +60,10 @@ void loadAppPreferences() {
     appPrefs.minimal_rssi = preferences.getInt(Keys::MIN_RSSI, -85);
 
     appPrefs.only_management_frames = preferences.getBool(Keys::ONLY_MGMT, true);
-    appPrefs.loop_delay = preferences.getUInt(Keys::LOOP_DELAY, 1000);
+    appPrefs.wifi_channel_dwell_time = preferences.getUInt(Keys::WIFI_CHANNEL_DWELL_TIME, 1000);
 
-    appPrefs.ble_scan_period = preferences.getUInt(Keys::BLE_SCAN_PERIOD, 30000);
-    appPrefs.ignore_random_ble_addresses = preferences.getBool(Keys::IGNORE_RANDOM, false);
+    appPrefs.ble_scan_delay = preferences.getUInt(Keys::BLE_SCAN_DELAY, 30);
+    appPrefs.ignore_random_ble_addresses = preferences.getBool(Keys::IGNORE_RANDOM, true);
     appPrefs.ble_scan_duration = preferences.getUInt(Keys::BLE_SCAN_DUR, 15);
     appPrefs.autosave_interval = preferences.getUInt(Keys::AUTOSAVE_INT, 60);
     appPrefs.passive_scan = preferences.getBool(Keys::PASSIVE_SCAN, false);
@@ -62,6 +72,11 @@ void loadAppPreferences() {
     String savedAddress = preferences.getString(Keys::AUTH_ADDR, "");
     strncpy(appPrefs.authorized_address, savedAddress.c_str(), sizeof(appPrefs.authorized_address) - 1);
     appPrefs.authorized_address[sizeof(appPrefs.authorized_address) - 1] = '\0';
+
+    appPrefs.cpu_speed = preferences.getUInt(Keys::CPU_SPEED, 80);
+    appPrefs.led_mode = preferences.getBool(Keys::LED_MODE, true);
+    appPrefs.wifiTxPower = preferences.getInt(Keys::WIFI_TX_POWER, WIFI_POWER_8_5dBm);
+    appPrefs.bleTxPower = preferences.getInt(Keys::BLE_TX_POWER, ESP_PWR_LVL_P9);
     
     preferences.end();
 
@@ -76,14 +91,18 @@ void saveAppPreferences() {
     preferences.putInt(Keys::OP_MODE, appPrefs.operation_mode);
     preferences.putInt(Keys::MIN_RSSI, appPrefs.minimal_rssi);
     preferences.putBool(Keys::ONLY_MGMT, appPrefs.only_management_frames);
-    preferences.putUInt(Keys::LOOP_DELAY, appPrefs.loop_delay);
-    preferences.putUInt(Keys::BLE_SCAN_PERIOD, appPrefs.ble_scan_period);
+    preferences.putUInt(Keys::WIFI_CHANNEL_DWELL_TIME, appPrefs.wifi_channel_dwell_time);
+    preferences.putUInt(Keys::BLE_SCAN_DELAY, appPrefs.ble_scan_delay);
     preferences.putBool(Keys::IGNORE_RANDOM, appPrefs.ignore_random_ble_addresses);
     preferences.putUInt(Keys::BLE_SCAN_DUR, appPrefs.ble_scan_duration);
     preferences.putUInt(Keys::AUTOSAVE_INT, appPrefs.autosave_interval);
     preferences.putBool(Keys::PASSIVE_SCAN, appPrefs.passive_scan);
     preferences.putBool(Keys::STEALTH_MODE, appPrefs.stealth_mode);
     preferences.putString(Keys::AUTH_ADDR, appPrefs.authorized_address);
+    preferences.putUInt(Keys::CPU_SPEED, appPrefs.cpu_speed);
+    preferences.putBool(Keys::LED_MODE, appPrefs.led_mode);
+    preferences.putInt(Keys::WIFI_TX_POWER, appPrefs.wifiTxPower);
+    preferences.putInt(Keys::BLE_TX_POWER, appPrefs.bleTxPower);
     preferences.end();
 
     Serial.println("App Preferences saved");
--- src/AppPreferences.h
+++ src/AppPreferences.h
@@ -15,12 +15,20 @@ struct AppPreferencesData {
     bool stealth_mode;
     // Wifi
     bool only_management_frames;
-    uint32_t loop_delay;
+    uint32_t wifi_channel_dwell_time;
+    uint8_t wifiTxPower;
+
     // BLE
-    uint32_t ble_scan_period;
+    uint32_t ble_scan_delay;
     bool ignore_random_ble_addresses;  
     uint32_t ble_scan_duration;   
     char authorized_address[18];
+    uint8_t bleTxPower;
+
+    // CPU
+    uint8_t cpu_speed;
+    // LED
+    uint8_t led_mode;
 };
 
 const int8_t OPERATION_MODE_OFF = 0;
@@ -34,14 +42,18 @@ namespace Keys {
     const char* const OP_MODE = "op_mode";
     const char* const MIN_RSSI = "min_rssi";
     const char* const ONLY_MGMT = "only_mgmt";
-    const char* const LOOP_DELAY = "loop_delay";
-    const char* const BLE_SCAN_PERIOD = "ble_period";
+    const char* const WIFI_CHANNEL_DWELL_TIME = "wifi_dwell";
+    const char* const BLE_SCAN_DELAY = "ble_delay";
     const char* const IGNORE_RANDOM = "ignore_rand";
     const char* const BLE_SCAN_DUR = "ble_dur";
     const char* const AUTOSAVE_INT = "autosave_int";
     const char* const PASSIVE_SCAN = "passive";
     const char* const STEALTH_MODE = "stealth";
     const char* const AUTH_ADDR = "auth_addr";
+    const char* const CPU_SPEED = "cpu_speed";
+    const char* const LED_MODE = "led_mode";
+    const char* const WIFI_TX_POWER = "wifi_tx_power";
+    const char* const BLE_TX_POWER = "ble_tx_power";
 }
 
 // Declaraciones de funciones
